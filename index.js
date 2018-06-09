455fcb151f52ff99ec3a1c422cd961416672d3f7
Sheraz Ali <sheraz.ali342@gmail.com>
2018-06-09 22:23:10 +0500

    Implementation of passport and google passport strategy using oauth20

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ hi: "Hello World!" });
-});
+// passport library know how to authenticate user but we need to tell or
+// register new style of strategy which is google-oauth20 in our case
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
+      console.log('done', done);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
